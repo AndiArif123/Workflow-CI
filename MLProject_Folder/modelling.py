ebbd84b4e3f039eebc4b4821b5e14d4269710410
AndiArif123 <secretemailfix@gmail.com>
2025-12-31 10:35:01 +0700

    Update modelling.py

--- MLProject_Folder/modelling.py
+++ MLProject_Folder/modelling.py
@@ -2,46 +2,33 @@ import os
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import dagshub
 import joblib
+import shutil
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-token = os.getenv("MLFLOW_TRACKING_PASSWORD") 
-repo_owner = 'AndiArif123'
-repo_name = 'Eksperimen_SML_Andi-Arif-Abdillah'
-
-os.environ['MLFLOW_TRACKING_USERNAME'] = repo_owner
-os.environ['MLFLOW_TRACKING_PASSWORD'] = token
-mlflow.set_tracking_uri(f"https://dagshub.com/{repo_owner}/{repo_name}.mlflow")
+os.environ['MLFLOW_TRACKING_USERNAME'] = 'AndiArif123'
+os.environ['MLFLOW_TRACKING_PASSWORD'] = os.getenv("MLFLOW_TRACKING_PASSWORD", "")
+mlflow.set_tracking_uri("https://dagshub.com/AndiArif123/Eksperimen_SML_Andi-Arif-Abdillah.mlflow")
 
 mlflow.sklearn.autolog()
 
 def run_modelling():
-    file_path = 'iris_preprocessing.csv'
-    
-    if not os.path.exists(file_path):
-        file_path = '../iris_preprocessing.csv'
-
-    if not os.path.exists(file_path):
-        return
-
-    df = pd.read_csv(file_path)
+    df = pd.read_csv('iris_preprocessing.csv')
     X = df.drop('target', axis=1)
     y = df['target']
-
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
     with mlflow.start_run(run_name="CI_Retraining_Model"):
         model = RandomForestClassifier(n_estimators=100, random_state=42)
         model.fit(X_train, y_train)
         
+        # Simpan file artefak untuk GitHub Artifacts
         joblib.dump(model, "model.pkl")
-        mlflow.log_artifact("model.pkl")
-        mlflow.sklearn.log_model(model, "tuned_iris_model")
-        mlflow.sklearn.save_model(model, "tuned_iris_model_local")
-
-
+        
+        if os.path.exists("model_output"):
+            shutil.rmtree("model_output")
+        mlflow.sklearn.save_model(model, "model_output")
 
 if __name__ == "__main__":
     run_modelling()
