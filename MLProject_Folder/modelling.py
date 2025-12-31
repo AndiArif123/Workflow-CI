3a17b9e06cacc9e5c7450e3f523369996966442d
AndiArif123 <secretemailfix@gmail.com>
2026-01-01 03:11:24 +0700

    Update modelling.py

--- MLProject_Folder/modelling.py
+++ MLProject_Folder/modelling.py
@@ -22,41 +22,40 @@ csv_path = 'iris_preprocessing.csv'
 if not os.path.exists(csv_path):
     raise FileNotFoundError(f"File {csv_path} tidak ditemukan!")
 
-with mlflow.start_run(nested=True):
-    df = pd.read_csv(csv_path)
-    X = df.drop('target', axis=1)
-    y = df['target']
+df = pd.read_csv(csv_path)
+X = df.drop('target', axis=1)
+y = df['target']
 
-    X_train, X_val, y_train, y_val = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
-    )
+X_train, X_val, y_train, y_val = train_test_split(
+    X, y, test_size=0.2, random_state=42, stratify=y
+)
 
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-    model.fit(X_train, y_train)
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_val)
-    acc = accuracy_score(y_val, y_pred)
+y_pred = model.predict(X_val)
+acc = accuracy_score(y_val, y_pred)
 
-    mlflow.log_param("n_estimators", 100)
-    mlflow.log_param("random_state", 42)
-    mlflow.log_metric("val_accuracy", acc)
+mlflow.log_param("n_estimators", 100)
+mlflow.log_param("random_state", 42)
+mlflow.log_metric("val_accuracy", acc)
 
-    report = classification_report(y_val, y_pred, output_dict=True)
-    with open("classification_report.json", "w") as f:
-        json.dump(report, f, indent=4)
-    mlflow.log_artifact("classification_report.json")
+report = classification_report(y_val, y_pred, output_dict=True)
+with open("classification_report.json", "w") as f:
+    json.dump(report, f, indent=4)
+mlflow.log_artifact("classification_report.json")
 
-    mlflow.sklearn.log_model(
-        sk_model=model,
-        artifact_path="model",
-        registered_model_name="IrisRandomForestModel"
-    )
+mlflow.sklearn.log_model(
+    sk_model=model,
+    artifact_path="model",
+    registered_model_name="IrisRandomForestModel"
+)
 
-    joblib.dump(model, "model.pkl")
-    mlflow.log_artifact("model.pkl")
+joblib.dump(model, "model.pkl")
+mlflow.log_artifact("model.pkl")
 
-    output_dir = "model_output"
-    if os.path.exists(output_dir):
-        shutil.rmtree(output_dir)
-    mlflow.sklearn.save_model(model, output_dir)
-    mlflow.log_artifact(output_dir, artifact_path="model_output_folder")
+output_dir = "model_output"
+if os.path.exists(output_dir):
+    shutil.rmtree(output_dir)
+mlflow.sklearn.save_model(model, output_dir)
+mlflow.log_artifact(output_dir, artifact_path="model_output_folder")
